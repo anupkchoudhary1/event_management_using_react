30c480d98625d2b18658a9d7d49e396703d7f176
anup kumar <anupchoudhary121@gmail.com>
2024-07-23 10:57:38 +0530

    save project

---
+++ event-management-main/frontend/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Welcome to Dream Maker!
+
+At Dream Maker, we specialize in crafting unforgettable moments for birthdays, weddings, anniversaries, and every celebration in between. With a passion for creativity and attention to detail, our team is dedicated to making your dream event a reality. Whether it's an intimate gathering or a grand affair, we're here to turn your vision into an extraordinary experience. Let us handle the planning while you focus on creating lasting memories. Trust us to bring your special moments to life, leaving you with cherished memories that will last a lifetime.
+
+
+      </p>
+    </section>
+  );
+};
+
+export default About;
